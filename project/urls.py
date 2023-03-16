f2660efc1aa70c93232fe2aabaee96071c49bcd4
mm-mazhar <mazqoty.01@gmail.com>
2023-03-16 21:18:24 +0300

    first commit

--- project/urls.py
+++ project/urls.py
@@ -29,4 +29,5 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+# #For Deployment
 urlpatterns += staticfiles_urlpatterns()

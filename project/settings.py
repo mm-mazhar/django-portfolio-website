c967f346bff9ec22f08c7a2f67ad44999c177d5b
mm-mazhar <mazqoty.01@gmail.com>
2023-03-16 20:15:50 +0300

    first commit

--- project/settings.py
+++ project/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print("Base DIR: ", BASE_DIR)
+# print("Base DIR: ", BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/

df2cf17221559500397fa5f2c311fd3fe7c3efc9
mm-mazhar <mazqoty.01@gmail.com>
2023-03-16 18:51:43 +0300

    first commit

--- project/wsgi.py
+++ project/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+# add the project path into the sys.path
+# sys.path.append()
+
+# add the virtual Environment site-packages path to the sys.path
+# sys.path.append(/Lib/site-packages)
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
+
+
+
+

ae7374b25735ddf99333004f3ceb6434313fe7fe
mm-mazhar <mazqoty.01@gmail.com>
2023-03-17 13:39:55 +0300

    first commit

--- project/settings.py
+++ project/settings.py
@@ -38,9 +38,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG')))
+#DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
